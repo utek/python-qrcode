b9bd0c7535bb9401f67b13d3ac032a8cdb70973f
utek <mail@utek.pl>
2012-01-19 14:13:16 +0100

    Added Cairo Image renderer

--- qrcode/renderers/r_cairo.py
+++ qrcode/renderers/r_cairo.py
@@ -1,23 +1,50 @@
-#Cairo renderer placeholder
+#import cairo
+import cairo
 
 
-def render(qrcode):
-    """
-    Cairo placeholder
-    """
+def prepare(qrcode):
     if qrcode.data_cache is None:
         qrcode.make()
-    offset = qrcode.border
+    offset = qrcode.border   # Spec says border should be at least four boxes wide
     pixelsize = (qrcode.modules_count + offset * 2) * qrcode.box_size
+    return (offset, pixelsize)
 
-    #im = Image.new("1", (pixelsize, pixelsize), "white")
-    #d = ImageDraw.Draw(im)
+def make(qrcode, surface, pixelsize, offset):
+    ctx = cairo.Context(surface)
+    pat_w = cairo.SolidPattern(1, 1, 1, 1)
+    pat_b = cairo.SolidPattern(0, 0, 0, 1)
+    ctx.rectangle(0, 0, pixelsize, pixelsize)
+    ctx.set_source(pat_w)#fill it white
+    ctx.fill()
+    ctx.set_source(pat_b)#paint it black
     for r in range(qrcode.modules_count):
         for c in range(qrcode.modules_count):
             if qrcode.modules[r][c]:
                 x = (c + offset) * qrcode.box_size
                 y = (r + offset) * qrcode.box_size
-                b = [(x, y),
-                    (x + qrcode.box_size - 1, y + qrcode.box_size - 1)]
-                #d.rectangle(b, fill="black")
-    return None
+                #cairo Rectangle is x0, y0, x1, y1
+                x1, y1 = (qrcode.box_size, qrcode.box_size)
+                ctx.rectangle(x, y, x1, y1)
+                ctx.fill()
+    return surface
+
+def render_image(qrcode):
+    """
+    Make a cairo ImageSurface version of QRCode
+
+    """
+    offset, pixelsize = prepare(qrcode)
+    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, pixelsize, pixelsize)
+    return make(qrcode, surface, pixelsize, offset)
+
+def render_svg(qrcode):
+    """
+    Make a cairo ImageSurface version of QRCode
+
+    """
+    offset, pixelsize = prepare(qrcode)
+    print offset, pixelsize
+    surface = cairo.SVGSurface(None, pixelsize, pixelsize)
+    return make(qrcode, surface, pixelsize, offset)
+
+render = render_image
